70376c3a25a55488c4b8be9b3744d4bc954e2e80
Skylar <skylarhimeno@gmail.com>
2019-02-13 09:20:51 -0700

    Started Creating Queue and Created FileController finished with hpp but not cpp

---
+++ DataStructures/Controller/Tools/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  DataStructures
+//
+//  Created by Himeno, Skylar on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ DataStructures/Controller/Tools/FileController.hpp
@@ -0,0 +1,40 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Himeno, Skylar on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "../Model/Linear/Array.hpp"
+#include "../Model/Linear/LinkedList.hpp"
+#include "../Model/NonLinear/BinarySearchTree.hpp"//ignore me
+
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    static LinkedList<CrimeData> readDataToList(string filename);
+    static BinarySearchTree<CrimeData> readDataToTree(string filename);//ignore
+    
+    static vector<Music> musicDataToArray(string filename);
+    
+    static Array<Music> musicDataToArray(string filename);
+    static LinkedList<Music> musicDataToList(string filename);
+    static Binary SearchTree<Music> musicDataToTree(string filename);//Keep on ignoring
+};
+
+#endif /* FileController_hpp */
