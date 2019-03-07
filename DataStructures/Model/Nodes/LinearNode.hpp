edbedf03b774cf4453aef18a2aeb39e9b386832c
Skylar <skylarhimeno@gmail.com>
2019-03-07 09:09:31 -0700

    Fixed DoubleNode and put in LinearNode in the right file

---
+++ DataStructures/Model/Nodes/LinearNode.hpp
@@ -0,0 +1,58 @@
+//
+//  LinearNode.hpp
+//  DataStructures
+//
+//  Created by Himeno, Skylar on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class LinearNode : public Node<Type>
+{
+protected:
+    LinearNode<Type> * next;
+public:
+    //Constructors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+    void setNextNode(LinearNode<Type> * next);
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+
+#endif /* LinearNode_hpp */
